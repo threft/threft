35919c45846bf142e6c9980679a77acdc5abd98b
GeertJohan <gjr19912@gmail.com>
2013-01-03 00:19:10 +0100

    Added comments for testing.

--- testfiles/test.thrift
+++ testfiles/test.thrift
@@ -1,7 +1,9 @@
 include test bla **this is ignored right now**
 cpp_include Both 'include' and 'cpp_include' header statements are being ignored by the parser.
-fdsa
-fdawef
-fdas
-fdsa
+// comment to be ignored
+# comment to be ignored
+fdsa // comment to be ignored
+fdawef # comment to be ignored
+fdas//no whitespace comment to be ignored
+fdsa#no whitespace comment to be ignored
 fdsa
